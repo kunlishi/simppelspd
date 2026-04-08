f7bd2fd74226932e298e64626bb90e8c651f3dd7
Ahmad Aikun <222312949@stis.ac.id>
2026-04-08 08:45:01 +0700

    Perbaikan notif dan tabel penindakan harian

--- resources/views/operasirutin/laporanrutin.blade.php
+++ resources/views/operasirutin/laporanrutin.blade.php
@@ -396,7 +396,13 @@
                                                     confirmButtonColor: '#d33',
                                                     cancelButtonColor: '#3085d6',
                                                     confirmButtonText: 'Ya, hapus!',
-                                                    cancelButtonText: 'Batal'
+                                                    cancelButtonText: 'Batal',
+                                                    cancelButtonText: 'Batal',
+                                                    customClass: {
+                                                        confirmButton: 'bg-red-600 text-white hover:bg-red-700 px-4 py-2 rounded ml-2',
+                                                        cancelButton: 'bg-green-600 text-white hover:bg-green-700 px-4 py-2 rounded'
+                                                    },
+                                                    buttonsStyling: false
                                                 }).then((result) => {
                                                     if (result.isConfirmed) {
                                                         // Submit form programmatically
